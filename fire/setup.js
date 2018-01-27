aeb6a97a382d7adc271e352b743d240579401245
Doris Cheng <dorcheng@umich.edu>
2018-01-27 14:11:32 -0500

    set up project

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyB5xZXjJCxTfzst0qe-IqfHbO-3JbvlrGo",
+  "databaseURL": "https://beyond-57904.firebaseio.com",
+  "storageBucket": "beyond-57904.appspot.com",
+  "authDomain": "beyond-57904.firebaseapp.com",
+  "messagingSenderId": "316017139089",
+  "projectId": "beyond-57904"
+});
+
